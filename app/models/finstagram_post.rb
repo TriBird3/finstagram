85e43b125525be13a67cec8f47759c7688c2cf7e
TriBird3 <trentpaisley@gmail.com>
2021-05-11 03:35:03 +0000

    Added days to humanized_time_ago

--- app/models/finstagram_post.rb
+++ app/models/finstagram_post.rb
@@ -10,9 +10,13 @@ class FinstagramPost < ActiveRecord::Base
     def humanized_time_ago
         time_ago_in_seconds = Time.now - self.created_at
         time_ago_in_minutes = time_ago_in_seconds / 60
+        time_ago_in_hours = time_ago_in_minutes / 60
 
-        if time_ago_in_minutes >= 60
-            "#{(time_ago_in_minutes / 60).to_i} hours ago"
+        if time_ago_in_hours >= 24
+            "#{(time_ago_in_hours / 24).to_i} days ago"
+        
+        elsif time_ago_in_minutes >= 60
+                "#{(time_ago_in_minutes / 60).to_i} hours ago"
         else
             "#{time_ago_in_minutes.to_i} minutes ago"
         end
